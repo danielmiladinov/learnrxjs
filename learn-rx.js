d9a1abfeee1de9d52c578c04e2266256838ad824
Daniel Miladinov <daniel.miladinov@gmail.com>
2015-07-05 18:21:31 -0400

    Solve Exercise 5

--- learn-rx.js
+++ learn-rx.js
@@ -133,3 +133,52 @@ Array.prototype.map = function (projectionFunction) {
 
   return results;
 };
+
+
+// Exercise 5: Use map() to project an array of videos into an array of {id,title} pairs
+
+// Let's repeat the exercise of collecting {id, title} pairs for each video in the newReleases array, but this time
+// we'll use our map function.
+function useMapToProjectIdAndTitlePairs () {
+  var newReleases = [
+    {
+      "id": 70111470,
+      "title": "Die Hard",
+      "boxart": "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
+      "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+      "rating": [4.0],
+      "bookmark": []
+    },
+    {
+      "id": 654356453,
+      "title": "Bad Boys",
+      "boxart": "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
+      "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+      "rating": [5.0],
+      "bookmark": [{ id:432534, time:65876586 }]
+    },
+    {
+      "id": 65432445,
+      "title": "The Chamber",
+      "boxart": "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
+      "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+      "rating": [4.0],
+      "bookmark": []
+    },
+    {
+      "id": 675465,
+      "title": "Fracture",
+      "boxart": "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
+      "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+      "rating": [5.0],
+      "bookmark": [{ id:432534, time:65876586 }]
+    }
+  ];
+
+  return newReleases.map(function (newRelase) {
+    return { id: newRelase.id, title: newRelase.title };
+  });
+}
+
+// Notice that map allows us to specify what projection we want to apply to an array,
+// but hides how the operation is carried out.
