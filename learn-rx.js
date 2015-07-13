9da5a0d840141f300411bbbda63bd50394144429
Daniel Miladinov <daniel.miladinov@gmail.com>
2015-07-12 22:33:23 -0400

    Solve Exercise 11

--- learn-rx.js
+++ learn-rx.js
@@ -406,3 +406,63 @@ Array.prototype.mergeAll = function() {
 
 // Merge is a very simple function, so much so that it may not be obvious yet how it can be combined with map()
 // to query a tree. Let's try an example...
+
+
+// Exercise 11: Use map() and mergeAll() to project and flatten the movieLists into an array of video ids
+
+// Hint: use two nested calls to map() and one call to mergeAll().
+function useMapAndMergeAllToFlattenTheMovieListsIntoAnArrayOfIds () {
+  var movieLists = [
+    {
+      name: "New Releases",
+      videos: [
+        {
+          "id": 70111470,
+          "title": "Die Hard",
+          "boxart": "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
+          "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+          "rating": 4.0,
+          "bookmark": []
+        },
+        {
+          "id": 654356453,
+          "title": "Bad Boys",
+          "boxart": "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
+          "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+          "rating": 5.0,
+          "bookmark": [{ id:432534, time:65876586 }]
+        }
+      ]
+    },
+    {
+      name: "Dramas",
+      videos: [
+        {
+          "id": 65432445,
+          "title": "The Chamber",
+          "boxart": "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
+          "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+          "rating": 4.0,
+          "bookmark": []
+        },
+        {
+          "id": 675465,
+          "title": "Fracture",
+          "boxart": "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
+          "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+          "rating": 5.0,
+          "bookmark": [{ id:432534, time:65876586 }]
+        }
+      ]
+    }
+  ];
+
+  return movieLists.map(function (movieList) {
+    return movieList.videos.map(function (video) {
+      return video.id;
+    });
+  }).mergeAll();
+}
+
+// Wow! Great work. Mastering the combination of map() and mergeAll() is key to effective functional programming. You're
+// half way there! Let's try a more complicated example...
