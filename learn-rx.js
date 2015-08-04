ad1822abb4214af6341fafb99005018223727cfc
Daniel Miladinov <daniel.miladinov@gmail.com>
2015-08-03 21:46:06 -0400

    Solve Exercise 34
    . . . with a little help from the internets!

--- learn-rx.js
+++ learn-rx.js
@@ -1838,3 +1838,52 @@ function spriteDragEvents (sprite, spriteContainer) {
 
 // Now we're really cooking. We just created a complex event with a few lines a code. We didn't have to deal with any
 // subscriptions objects, or write any stateful code whatsoever. Let's try something a little harder.
+
+
+// Exercise 34: Improving our mouse drag event
+
+// Our mouse drag event is a little too simple. Notice that when we drag around the sprite, it always positions itself
+// at the top-left corner of the mouse. Ideally we'd like our drag event to offset its coordinates, based on where the
+// mouse was when the mouse down event occurred. This will make our mouse drag more closely resemble moving a real
+// object with our finger.
+
+// Let's see if you can adjust the coordinates in the mouse drag event, based on the mousedown location on the sprite.
+// The mouse events are sequences, and they look something like this:
+
+var spriteContainerMouseMoves =
+  seq([ {x: 200, y: 400, offsetX: 10, offsetY: 15},,,{x: 210, y: 410, offsetX: 20, offsetY: 26},,, ]);
+
+// Each item in the mouse event sequences contains an x, y value that represents that absolute location of the mouse
+// event on the page. The moveSprite() function uses these coordinates to position the sprite. Each item in the sequence
+// also contains a pair of offsetX and offsetY properties that indicate the position of the mouse event relative to the
+// event target.
+function aMoreSophisticatedDragEvent (sprite, spriteContainer) {
+  // All of the mouse event sequences look like this:
+  // seq([ {pageX: 22, pageY: 3423, offsetX: 14, offsetY: 22} ,,, ])
+  var spriteMouseDowns = Observable.fromEvent(sprite, "mousedown"),
+    spriteContainerMouseMoves = Observable.fromEvent(spriteContainer, "mousemove"),
+    spriteContainerMouseUps = Observable.fromEvent(spriteContainer, "mouseup"),
+  // Create a sequence that looks like this:
+  // seq([ {pageX: 22, pageY:4080 },,,{pageX: 24, pageY: 4082},,, ])
+    spriteMouseDrags =
+      // For every mouse down event on the sprite...
+      spriteMouseDowns.
+        flatMap(function(contactPoint) {
+          // ...retrieve all the mouse move events on the sprite container...
+          return spriteContainerMouseMoves.
+            // ...until a mouse up event occurs.
+            takeUntil(spriteContainerMouseUps).
+          map(function (mouseMove) {
+            return {
+              pageX: mouseMove.pageX - contactPoint.offsetX,
+              pageY: mouseMove.pageY - contactPoint.offsetY
+            };
+          });
+        });
+
+  // For each mouse drag event, move the sprite to the absolute page position.
+  spriteMouseDrags.forEach(function(dragPoint) {
+    sprite.style.left = dragPoint.pageX + "px";
+    sprite.style.top = dragPoint.pageY + "px";
+  });
+}
