04e88838f3e7e7f0e00e0b2fc7767c92879b5972
Daniel Miladinov <daniel.miladinov@gmail.com>
2015-08-04 20:09:12 -0400

    "Solve" Exercise 35

--- learn-rx.js
+++ learn-rx.js
@@ -1887,3 +1887,24 @@ function aMoreSophisticatedDragEvent (sprite, spriteContainer) {
     sprite.style.top = dragPoint.pageY + "px";
   });
 }
+
+
+// Exercise 35: HTTP requests
+
+// Events aren't the only source of asynchronous data in an application. There's also HTTP requests. Most of the time
+// HTTP requests are exposed via a callback-based API. To receive data asynchronously from a callback-based API,
+// the client typically passes a success and error handler to the function. When the asynchronous operation completes,
+// the appropriate handler is called with the data. In this exercise we'll use jQuery's getJSON api to asynchronously
+// retrieve data.
+
+function getJSONWithJQuery ($) {
+  $.getJSON(
+    "http://api-global.netflix.com/queue", {
+    success: function (json) {
+      alert("Data has arrived.");
+    },
+    error: function (ex) {
+      alert("There was an error.")
+    }
+  });
+}
