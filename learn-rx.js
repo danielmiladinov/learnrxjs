1848a0949418f6b715eb85874fc793418df8167a
Daniel Miladinov <daniel.miladinov@gmail.com>
2015-08-10 21:13:30 -0400

    Solve Exercise 42

--- learn-rx.js
+++ learn-rx.js
@@ -2236,3 +2236,26 @@ function autocompleteBox(getSearchResultSet, keyPresses, textBox) {
 
 // Now that we're able to query with our throttled input, you'll still notice one slight problem. If you hit your arrow
 // keys or any other non character key, the request will still fire. How do we prevent that?
+
+
+// Exercise 42: Distinct Until Changed Input
+
+// You'll notice in the previous exercise that if you pressed your arrow keys while inside the textbox, the query will
+// still fire, regardless of whether the text actually changed or not. How do we prevent that? The distinctUntilChanged
+// filters out successive repetitive values.
+
+
+// seq([1,,,1,,,3,,,3,,,5,,,1,,,]).distinctUntilChanged() ===
+// seq([1,,,,,,,3,,,,,,,5,,,1,,,]);
+function distinctUntilChangedInput(keyPresses, isAlpha) {
+
+  return keyPresses.
+    map(function (e) { return String.fromCharCode(e.keyCode); }).
+    filter(function (character) { return isAlpha(character); }).
+    distinctUntilChanged().
+    scan('', function (stringSoFar, character) {
+      return stringSoFar + character;
+    });
+}
+
+// Now that we know how to get only the distinct input, let's see how it applies to our autocomplete example...
