e8dfde2d6c9bff48818fba8cdd58ce25caf7f1f5
Daniel Miladinov <daniel.miladinov@gmail.com>
2015-08-11 23:18:40 -0400

    Solve Exercise 43

--- learn-rx.js
+++ learn-rx.js
@@ -2259,3 +2259,37 @@ function distinctUntilChangedInput(keyPresses, isAlpha) {
 }
 
 // Now that we know how to get only the distinct input, let's see how it applies to our autocomplete example...
+
+
+// Exercise 43: Autocomplete Box Part 2: Electric Boogaloo
+
+// In the previous version of the autocomplete box, there were two bugs:
+// * Multiple successive searches are made for the same string
+// * Attempts are made to retrieve results for an empty string.
+
+// The example below is the same as above, but this time, fix the bugs!
+
+// getSearchResultSet('react') === seq[,,,["reactive", "reaction","reactor"]]
+// keyPresses === seq['r',,,,,'e',,,,,,'a',,,,'c',,,,'t',,,,,]
+function autocompletBoxBugsFixed(getSearchResultSet, keyPresses, textBox) {
+
+  var getSearchResultSets =
+    keyPresses.
+      map(function () {
+        return textBox.value;
+      }).
+      throttle(1000).
+
+      distinctUntilChanged().
+
+      filter(function(str) { return str.length > 0; }).
+
+      flatMap(function (text) {
+        return getSearchResultSet(text).takeUntil(keyPresses);
+      });
+
+  return getSearchResultSets;
+}
+
+// With just this little amount of code, we're able to produce a fully functioning autocomplete scenario. But there are
+// other outstanding questions, such as error handling. How can we handle failure and retry if necessary?
